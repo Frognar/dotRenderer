8ebfc43dae67d6aa5df76daa48387de81636b3c5
frognar <sebastian.przyszlak@gmail.com>
2025-08-08 11:47:07 +0200

    Refactor `ITemplate` interface to replace non-generic definition with a strongly-typed generic version for improved type safety and flexibility.

--- src/dotRenderer/ITemplate.cs
+++ src/dotRenderer/ITemplate.cs
@@ -1,9 +1,5 @@
 namespace dotRenderer;
 
-public interface ITemplate
-{
-    string Render(IReadOnlyDictionary<string, object> model);
-}
 public interface ITemplate<TModel>
 {
     string Render(TModel model);
